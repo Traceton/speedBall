c9b410bf69ee0407bab0fbaea79a3cfc4fdf84fe
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-07-13 00:23:44 -0400

    created a basic game loop/draw/update functions.

--- game.js
+++ game.js
@@ -1,6 +1,30 @@
 import * as elements from "./elements.js";
 
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+let context = canvas.getContext("2d");
+let x = 0;
+let y = 0;
+
+let init = () => {
+  window.requestAnimationFrame(gameLoop);
+};
+
+let gameLoop = () => {
+  update();
+  draw();
+
+  window.requestAnimationFrame(gameLoop);
+};
+
+let draw = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = `#ff8080`;
+  context.fillRect(x, y, 150, 150);
+};
+
+let update = () => {
+  x += 5;
+  y += 7;
+};
+
+window.onload = init;
